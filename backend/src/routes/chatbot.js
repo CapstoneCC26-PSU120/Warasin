e65226e000eea0a15cbe28d270b459b031bc920b
ismanby <ismaanby@gmail.com>
2026-05-11 08:07:40 +0700

    feat: add delete history endpoint and update history response

--- backend/src/routes/chatbot.js
+++ backend/src/routes/chatbot.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/authMidleware.js";
-import { submitAnswer, getHistory } from "../controllers/chatbotController.js";
+import { submitAnswer, getHistory, deleteHistory } from "../controllers/chatbotController.js";
 import router from "./auth.js";
 
 router.post("/answer", authMiddleware, submitAnswer);
 router.get("/history", authMiddleware, getHistory);
+router.delete("/history/:id", authMiddleware, deleteHistory);
 
 export default router;
