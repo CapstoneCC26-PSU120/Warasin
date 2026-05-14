b0c65c5fd91df167cb8027fa59a6e8a6cbd1aa63
ismanby <ismaanby@gmail.com>
2026-05-14 19:50:52 +0700

    Add get history by id endpoint

--- backend/src/routes/chatbot.js
+++ backend/src/routes/chatbot.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/authMidleware.js";
-import { submitAnswer, getHistory, deleteHistory } from "../controllers/chatbotController.js";
+import { submitAnswer, getHistory, getHistoryById, deleteHistory } from "../controllers/chatbotController.js";
 import router from "./auth.js";
 
 router.post("/answer", authMiddleware, submitAnswer);
 router.get("/history", authMiddleware, getHistory);
+router.get("/history/:id", authMiddleware, getHistoryById)
 router.delete("/history/:id", authMiddleware, deleteHistory);
 
 export default router;
